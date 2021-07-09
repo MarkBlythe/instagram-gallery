21f20c600663d5efd6cb47c81d9276f53d015624
Mark Blythe <mark.blythe@mymhealth.com>
2021-07-09 22:55:17 +0100

    Added MIT license.

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import './styles.scss';
-interface GalleryProps {
-    accessToken: string;
-    count: number;
-}
-export declare const InstagramGallery: (props: GalleryProps) => JSX.Element;
-export {};
