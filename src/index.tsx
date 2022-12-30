334089fd2bc89032a2fa28e8f216cfd9d4e42e72
Mark Blythe <mark.blythe@mymhealth.com>
2022-12-30 15:29:50 +0000

    feat: 🎸 added better error handling to component
    no longer breaks everything and provides a message in place

--- src/index.tsx
+++ src/index.tsx
@@ -7,25 +7,26 @@ interface GalleryProps {
 }
 
 export const InstagramGallery = (props: GalleryProps) => {
-    const [loading, setLoading] = useState<Boolean>(true);
-    const [error, setError] = useState<Boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>();
     const [instagramData, setInstagramData] = useState<any>(null);
+    const [instagramGalleryData, setInstagramGalleryData] = useState<any[]>([]);
     const [usePagination, setUsePagination] = useState<boolean>(false);
     const [paginationNextUrl, setPaginationNextUrl] = useState<string>("");
     const [paginationPrevUrl, setPaginationPrevUrl] = useState<string>("");
 
     const fetchInstagramData = (url: string) => {
         fetch(url)
-            .then((response) => {
-                return response.json();
-            })
+            .then((response) => response.json())
             .then((data) => {
-                console.log({ data });
-                if (data.hasOwnProperty("error")) {
+                if (data.error) {
                     setLoading(false);
+                    setErrorMessage(data.error.message);
                     setError(true);
                 } else {
                     setInstagramData(data);
+                    setInstagramGalleryData(data.data);
                     setLoading(false);
                 }
             })
@@ -62,62 +63,64 @@ export const InstagramGallery = (props: GalleryProps) => {
         fetchInstagramData(paginationPrevUrl);
     };
 
-    if (loading) {
-        return <div className="instagram-gallery">LOADING...</div>;
-    }
-
-    if (error) {
+    if (error && errorMessage) {
         return (
             <div className="instagram-gallery">
-                Ruh Roh!. Something went wrong.
+                <p>InstagramGallery: Something went wrong.</p>
+                <p>{errorMessage}</p>
             </div>
         );
     }
 
+    if (loading) {
+        return <div className="instagram-gallery">LOADING...</div>;
+    }
+
     return (
         <div className="instagram-gallery">
-            {instagramData.data
-                .slice(0, props.count)
-                .map((item: any, index: any) => (
-                    <div key={index} className="instagram-item">
-                        <a
-                            key={index}
-                            href={item.permalink}
-                            className="instagram-link"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            {item.media_type === "IMAGE" ||
-                            item.media_type === "CAROUSEL_ALBUM" ? (
-                                <img
-                                    className="instagram-image"
-                                    key={index}
-                                    src={item.media_url}
-                                    alt={item.caption}
-                                />
-                            ) : (
-                                <video
-                                    src={item.media_url}
-                                    className="instagram-image"
-                                    key={index}
-                                    autoPlay
-                                    muted
-                                    playsInline
-                                    controls
-                                >
-                                    <source
+            {instagramGalleryData &&
+                instagramGalleryData
+                    .slice(0, props.count)
+                    .map((item: any, index: any) => (
+                        <div key={index} className="instagram-item">
+                            <a
+                                key={index}
+                                href={item.permalink}
+                                className="instagram-link"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                {item.media_type === "IMAGE" ||
+                                item.media_type === "CAROUSEL_ALBUM" ? (
+                                    <img
+                                        className="instagram-image"
+                                        key={index}
                                         src={item.media_url}
-                                        type="video/mp4"
+                                        alt={item.caption}
                                     />
-                                    <source
+                                ) : (
+                                    <video
                                         src={item.media_url}
-                                        type="video/webm"
-                                    />
-                                </video>
-                            )}
-                        </a>
-                    </div>
-                ))}
+                                        className="instagram-image"
+                                        key={index}
+                                        autoPlay
+                                        muted
+                                        playsInline
+                                        controls
+                                    >
+                                        <source
+                                            src={item.media_url}
+                                            type="video/mp4"
+                                        />
+                                        <source
+                                            src={item.media_url}
+                                            type="video/webm"
+                                        />
+                                    </video>
+                                )}
+                            </a>
+                        </div>
+                    ))}
             {usePagination && (
                 <div className="pagination">
                     {paginationPrevUrl && (
